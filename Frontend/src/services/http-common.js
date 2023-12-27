95c12de15cbf4d84631bdeeed1584ce43fbdee61
Tudzy <narongsak05n@gmail.com>
2023-12-27 23:02:36 +0700

    set baseURL

---
+++ Frontend/src/services/http-common.js
@@ -0,0 +1,20 @@
+const { default: axios } = require("axios")
+
+const defaultOptoins = {
+    baseURL: "http://localhost:4000/",
+    headers: {
+        "Content-Type": "application/json"
+    }
+}
+
+// create instanc
+let instance = axios.create(defaultOptoins)
+
+// set the auth token for my request
+instance.interceptors.request.use(function (config) {
+    const token = localStorage.getItem("token");
+    config.headers.Authorization = token ? `Bearer${token}` : ""
+    return config
+})
+
+export default instance
