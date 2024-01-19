3c4b194427a5a9947e8fd7cf46d3020e443df985
Phoomss <narongsak05n@gmail.com>
2024-01-19 22:52:47 +0700

    fig model

--- Backend/controllers/orderMasterController.js
+++ Backend/controllers/orderMasterController.js
@@ -73,3 +73,20 @@ const createOrderMaster = async (req, res) => {
 module.exports = {
     createOrderMaster
 }
+
+
+// app.post('/createOrder', async (req, res) => {
+//     try {
+//       const { stuID, teamID, teachID, dateBuy, timeBuy, pickupDate, pickupTime, price, status, depositPrice, remainingPrice } = req.body;
+//       const order = await Order.create({ stuID, teamID, teachID, dateBuy, timeBuy, pickupDate, pickupTime, price, status, depositPrice, remainingPrice });
+//       const orderDetails = req.body.orderDetails;
+//       for (let i = 0; i < orderDetails.length; i++) {
+//         const { cakeID, totalCake, totalPoundsAmount, totalPrice } = orderDetails[i];
+//         await OrderDetails.create({ orderID: order.id, cakeID, totalCake, totalPoundsAmount, totalPrice });
+//       }
+//       res.status(201).json(order);
+//     } catch (error) {
+//       console.error(error);
+//       res.status(500).json({ message: 'Server Error' });
+//     }
+//   });
--- Backend/models/orderModel.js
+++ Backend/models/orderModel.js
@@ -1,10 +1,46 @@
 module.exports = (sequelize, DataTypes) => {
     const Order = sequelize.define('orders', {
-        orderMasterID: {
+        stuID: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        },
+        teamID: {
             type: DataTypes.INTEGER,
             allowNull: true
         },
         teachID: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        },
+        dateBuy: {
+            type: DataTypes.DATEONLY,
+            allowNull: false
+        },
+        timeBuy: {
+            type: DataTypes.TIME,
+            allowNull: false
+        },
+        pickupDate: {
+            type: DataTypes.DATEONLY,
+            allowNull: false
+        },
+        pickupTime: {
+            type: DataTypes.TIME,
+            allowNull: false
+        },
+        price: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        },
+        status: {
+            type: DataTypes.ENUM("จ่ายเต็มจำนวน", "จ่ายแบบมัดจำ"),
+            allowNull: true
+        },
+        depositPrice: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        },
+        remainingPrice: {
             type: DataTypes.INTEGER,
             allowNull: true
         },
