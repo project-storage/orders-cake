7899f40854f1441cae712001a286d461eae92e9d
Tudzy <narongsak5az@gmail.com>
2024-05-22 11:50:55 +0700

    set redux to group

--- frontend-test-2/src/app/store.js
+++ frontend-test-2/src/app/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import cakeReducer from '../slices/cakeSlice';
 import departmentReducer from '../slices/departmentSlice';
 import degreeReducer from '../slices/degreeSlice'
+import groupReducer from '../slices/groupSlice'
 
 export const store = configureStore({
   reducer: {
     cakes: cakeReducer,
     departments: departmentReducer,
-    degrees: degreeReducer
+    degrees: degreeReducer,
+    group: groupReducer
   },
 });
