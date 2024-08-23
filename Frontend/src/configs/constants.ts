de68b3d5459ee8c9207fff8249be7622e4169876
Phoomss <narongsak5az@gmail.com>
2024-08-23 22:54:41 +0700

    change constants

--- Frontend/src/configs/constants.ts
+++ Frontend/src/configs/constants.ts
@@ -4,45 +4,26 @@ export const SYSTEM_NAME = "ระบบสั่งจองเค้ก";
 // super-admin Routes
 const BASE_PATH = "/super-admin/";
 export const DASHBOARD_PATH = BASE_PATH + "dashboard";
-export const ORDER_PATH = BASE_PATH + "order";
+export const ORDER_PATH = BASE_PATH + "orders";
 // cake path
-export const CAKE_PATH = BASE_PATH + "cake";
+export const CAKE_PATH = BASE_PATH + "cakes";
 export const UPDATE_CAKEA_PATH = CAKE_PATH + "/update";
 // group
-export const GROUP_PATH = BASE_PATH + "group";
+export const GROUP_PATH = BASE_PATH + "groups";
 export const UPDATE_GROUP_PATH = GROUP_PATH + "/update";
 // degree
-export const DEGREE_PATH = BASE_PATH + "degree";
+export const DEGREE_PATH = BASE_PATH + "degrees";
 export const UPDATE_DEGREE_PATH = DEGREE_PATH + "/update";
 // department
-export const DEPARTMENT_PATH = BASE_PATH + "department";
+export const DEPARTMENT_PATH = BASE_PATH + "departments";
 export const UPDATE_DEPARTMENT_PATH = DEPARTMENT_PATH + "/update";
 // users
 export const ALL_USER_PATH = BASE_PATH + "users";
-export const UPDATE_USER = ALL_USER_PATH + "users";
+export const UPDATE_USER = ALL_USER_PATH + "/update";
 // user info
-export const USERINFO_PATH = BASE_PATH + "users";
+export const USERINFO_PATH = BASE_PATH + "user-info";
 
 // tearcher routes
 const TEACHER_PATH = "/teacher/";
 export const DASHBOARD_TEACHERPATH = TEACHER_PATH + "dashboard";
 
-// System Constants
-// export const SYSTEM_NAME = 'React Vite Strapi'
-
-// Backend Page Title
-export const DASHBOARD_TITLE = 'Dashboard'
-export const PRODUCT_TITLE = 'Product'
-export const REPORT_TITLE = 'Report'
-export const SETTING_TITLE = 'Setting'
-
-// Backend Routes
-// const BASE_PATH = '/backend/'
-// export const DASHBOARD_PATH = BASE_PATH +'dashboard'
-export const PRODUCT_PATH = BASE_PATH + 'product'
-export const REPORT_PATH = BASE_PATH + 'report'
-export const SETTING_PATH = BASE_PATH + 'setting'
-
-// Profile Name and Role
-export const PROFILE_NAME = 'Samit Koyom'
-export const PROFILE_ROLE = 'Administrator'
